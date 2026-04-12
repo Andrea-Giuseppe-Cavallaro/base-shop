b7aab266aa3449e60b8d5fa559c6fea32a341c82
Andrea-Giuseppe-Cavallaro <adenor88@gmail.com>
2026-04-12 17:24:52 +0200

    feat: update product management with stock attribute and new product form

--- src/Service/Product.php
+++ src/Service/Product.php
@@ -8,22 +8,40 @@ class Product{
 
     private string $name;
     private float $price;
+    private ?string $description;
+    private int $stock;
     private LoggerInterface $logger;
 
-    public function __construct(string $name, float $price, LoggerInterface $logger)
+    public function __construct(
+        string $name, 
+        float $price, 
+        ?string $description, 
+        int $stock, 
+        LoggerInterface $logger
+        )
     {
         $this->name = $name;
         $this->price = $price;
+        $this->description = $description;
+        $this->stock = $stock;
         $this->logger = $logger;
 
         $this->logger->info("new product created", [
             "name" => $name,
             "price" => $price,
+            "description" => $description,
+            "stock" => $stock,
         ]);
     }
 
     public function describe(): string {
-        $message = $this->name . " cost " . $this->price;
+        $message = $this->name . 
+            " cost " . 
+            $this->price . 
+            "description: " . 
+            $this->description . 
+            "in stock: " . 
+            $this->stock;
         return $message;
     }
 }
